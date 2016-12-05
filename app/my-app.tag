ac80c3ddd65ee9593df3eb942533eff8022a2cba
Risa Harada <supikateriburu@gmail.com>
2016-12-05 12:51:50 +0900

    :sparkles: Totally works

---
+++ app/my-app.tag
@@ -0,0 +1,6 @@
+<my-app>
+  <h1>{title}</h1>
+<script>
+  this.title = "hello world"
+</script>
+</my-app>
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,6 +4,6 @@ riot.mount( 'starwars-yeah', {
     items : [
       { url: 'people/1/' },
       { url: "people/2/" },
-      { url: "people/5/" }
+      { url: "people/3/" }
     ]
 } )
